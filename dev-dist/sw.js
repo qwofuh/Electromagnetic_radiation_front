5b58bfecd924f4cff42191d7c74fc685b3028559
qwofuh <boguslavskiyfa@mail.ru>
2025-12-13 23:12:40 +0300

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "78b26cc371b74221c78b5a9f5704be5f"
   }, {
     "url": "index.html",
-    "revision": "0.5es1jur8ako"
+    "revision": "0.g1tog34q64"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
